04a5b4abe680863a123b778b659e23f653245176
Jason Dryhurst-Smith <jason@jasonds.co.uk>
2016-10-15 12:56:58 +0100

    update the main packages

--- dist/fluent-fix.spec.js
+++ dist/fluent-fix.spec.js
@@ -343,7 +343,7 @@ describe('Fixture builder', function () {
                 something: 5
             });
 
-            testClass = functionFixture.builder().withSomething(new FluentFix.Generator.For.String({ "default": "SOME_TEST" })).build();
+            testClass = functionFixture.builder().withSomething(new FluentFix.Generator.For.String({ default: "SOME_TEST" })).build();
         });
 
         it('will create a real object', function () {
@@ -384,13 +384,13 @@ describe('Fixture builder', function () {
 
 'use strict';
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 describe('Generators for fixture values', function () {
 
@@ -457,7 +457,7 @@ describe('Generators for fixture values', function () {
         describe('simple object', function () {
 
             beforeEach(function () {
-                testClass = new FluentFix.Generator.For.Boolean({ 'default': true }).generate();
+                testClass = new FluentFix.Generator.For.Boolean({ default: true }).generate();
                 testClassSimple = new FluentFix.Generator.For.Boolean().generate();
             });
 
@@ -476,7 +476,7 @@ describe('Generators for fixture values', function () {
         describe('simple object', function () {
 
             beforeEach(function () {
-                testClass = new FluentFix.Generator.For.Number({ 'default': 5 }).generate();
+                testClass = new FluentFix.Generator.For.Number({ default: 5 }).generate();
                 testClassSimple = new FluentFix.Generator.For.Number().generate();
                 testClassComplex = new FluentFix.Generator.For.Number({ min: 10, max: 15, sequential: true });
             });
@@ -518,7 +518,7 @@ describe('Generators for fixture values', function () {
         describe('simple object', function () {
 
             beforeEach(function () {
-                testClass = new FluentFix.Generator.For.String({ 'default': "SOME_TEST" }).generate();
+                testClass = new FluentFix.Generator.For.String({ default: "SOME_TEST" }).generate();
                 testClassSimple = new FluentFix.Generator.For.String("SOME_TEST").generate();
                 testClassComplex = new FluentFix.Generator.For.String({ min: 10, max: 15 });
             });
@@ -670,15 +670,16 @@ describe('Generators for fixture values', function () {
             test: 'TEST_VALUE'
         };
 
-        var Test = (function (_FluentFix$Generator$Abstract) {
-            _inherits(Test, _FluentFix$Generator$Abstract);
+        var Test = function (_FluentFix$Generator$) {
+            _inherits(Test, _FluentFix$Generator$);
 
             function Test() {
                 _classCallCheck(this, Test);
 
-                _get(Object.getPrototypeOf(Test.prototype), 'constructor', this).call(this);
+                var _this = _possibleConstructorReturn(this, (Test.__proto__ || Object.getPrototypeOf(Test)).call(this));
 
-                this.name = "TEST_GENERATOR";
+                _this.name = "TEST_GENERATOR";
+                return _this;
             }
 
             _createClass(Test, [{
@@ -694,7 +695,7 @@ describe('Generators for fixture values', function () {
             }]);
 
             return Test;
-        })(FluentFix.Generator.Abstract);
+        }(FluentFix.Generator.Abstract);
 
         beforeEach(function () {
             FluentFix.Generator.addGenerator(Test);
